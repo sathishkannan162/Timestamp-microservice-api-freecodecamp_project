c6832470b33d64718391cbb4a7528a21beac1956
sathishkannan162 <sathishkannan162@gmail.com>
2022-10-21 20:05:06 +0530

    fix: add port to .env

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
+
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
